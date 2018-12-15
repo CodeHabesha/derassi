ed224d4b59db978c6bc9ffa916d0df375e412a61
fasil <fasil@fasils-MacBook-Pro.local>
2018-12-15 12:38:24 -0800

    working on paging exp

--- src/components/pages.js
+++ src/components/pages.js
@@ -5,10 +5,10 @@ import Paragraph from './paragraph'
 import onChange from '../eventHandlers/onChange'
 import onKeyDown from '../eventHandlers/onKeyDown'
 import onKeyPress from '../eventHandlers/onKeyPress'
-import { element } from "prop-types"
+import { element, node } from "prop-types"
 import uniqid from 'uniqid'
 
-
+import sanitizeHtml from 'sanitize-html'
 
 
 
@@ -60,6 +60,10 @@ class Pages extends React.Component {
 
         
        }
+
+       componentDidMount(){
+         
+       }
       
        handleKeyDown = (e) => onKeyDown(e,this)
        handleChange = (e) => onChange(e,this)
@@ -75,26 +79,23 @@ class Pages extends React.Component {
             
         }
 
-        removePage(e){
+        removePage = (e) => {
             let pageId = e.target.id
-            console.log( " trying to remove")
+        
             let previous = -1; 
             let pages = this.state.pages.filter( (page,i) => { previous = i - 1 ;return(page.props.id !== pageId )})
             let prevPage = pages[previous]
-            
-            
-
-            console.log(pages)
+        
             this.setState({pages: pages})
             this.forceUpdate()
-            let pid = pages[previous].props.id
-            console.log("...focus back. id.", pid)
-            let prevEl = document.getElementById(pid)
-            let len = prevEl.innerHTML.length
-            setCaretPosition(prevEl, 1000)
-            //console.log(">> p elemnt ", p.element);
-            //prevPage.element.focus();
 
+            let pid = prevPage.props.id
+            let prevEl = document.getElementById(pid)
+            let node = prevEl.childNodes[0]
+            
+             // you need to set focus here before replacing carot
+             node.focus()
+             replaceCaret(node)
             
         }
 
@@ -119,23 +120,44 @@ class Pages extends React.Component {
 }
 
 
-// based on ; https://codepen.io/chrisshaw/pen/yNOVaz
-function setCaretPosition(ctrl, pos) {
-    console.log(" carot postions ", ctrl, pos)
-    // Modern browsers
-    if (ctrl.setSelectionRange) {
-      ctrl.focus();
-      ctrl.setSelectionRange(pos, pos);
+const findLastTextNode =  (node) => {
+    if (node.nodeType === Node.TEXT_NODE) return node;
+    let children = node.childNodes;
+    for (let i = children.length-1; i>=0; i--) {
+      let textNode = findLastTextNode(children[i]);
+      if (textNode !== null) return textNode;
+    }
+    return null;
+  }
+
+  const replaceCaret = (el /*:HTMLElement*/)  => {
     
-    // IE8 and below
-    } else if (ctrl.createTextRange) {
-      var range = ctrl.createTextRange();
+    // Place the caret at the end of the element
+    const target = findLastTextNode(el).parentNode;
+    
+    // do not move caret if element was not focused
+    const isTargetFocused = document.activeElement === target;
+    
+    if (target/*&& target.nodeValue !== null && isTargetFocused */) {
+      var range = document.createRange();
+      var sel = window.getSelection();
+      
+
+      let html = target.innerHTML
+      console.log(html)
+
+      //TODO: figur out how to find the right legth of innnerHTML to replace 0 in setStart(targe, 0)
+      range.setStart(target, 0 ); 
       range.collapse(true);
-      range.moveEnd('character', pos);
-      range.moveStart('character', pos);
-      range.select();
+      sel.removeAllRanges();
+      sel.addRange(range);
+      if (el instanceof HTMLElement) el.focus();
     }
   }
+  
+
+
+  
 
 
 export default Pages;
--- src/components/paragraph.js
+++ src/components/paragraph.js
@@ -1,19 +1,27 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { timingSafeEqual } from "crypto";
 
 
 class Paragraph extends React.Component {
      constructor(props){
         super(props)
+        this.handleFocus = this.handleFocus.bind(this)
      }
      componentDidMount(){
         this.element.focus();
         this.element.className = "paragraph"
         //this.element.addEventListener("input", this.props.onChange)
      }
+
+     handleFocus = (e) => {
+         console.log("pragraph focused... ", this.props.id, "...", e)
+         //this.element.value = this.element.value
+     }
+
     render(){
         return (
-            <div contentEditable = {true} id={this.props.id} ref={ (element) => this.element = element } onKeyDown={this.props.onKeyDown} onKeyPress={this.props.onKeyPress}> </div>
+            <div contentEditable = {true} onFocus={this.handleFocus} id={this.props.id} ref={ (element) => this.element = element } onKeyDown={this.props.onKeyDown} onKeyPress={this.props.onKeyPress}> </div>
         )
     }
    
